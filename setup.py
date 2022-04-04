52f2e1920b01f92cdb822d14f25baffe9dad7d39
TigiGln <thierry.galliano@etu.univ-amu.fr>
2022-04-04 19:14:22 +0200

    point caracteristiques and pick event en cours

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description = "Tools for extracting, analyzing and classifying optical tweezer data curves",
     long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     long_description_content_type = 'text/markdown',
-    url = 'https://github.com/TigiGln/stage_M2.git',
+    url = 'https://github.com/TigiGln/OTAnalysis.git',
     License= 'GPLv3+',
     classifiers=classifiers,
     keywords='optical tweezers',
