6ca874a541685a88348ac902d05bec2017ade2ba
TigiGln <thierry.galliano@etu.univ-amu.fr>
2022-05-24 15:09:36 +0200

    add docs

---
+++ otanalysis/__main__.py
@@ -0,0 +1,32 @@
+"""
+main module to launch the optical tweezers curve analysis and classification tool
+"""
+import os
+import sys
+from time import time
+from pathlib import Path
+from PyQt5.QtWidgets import QApplication
+from otanalysis.view.mainview import View
+from otanalysis.controller.controller import Controller
+
+
+def main():
+    """
+    Launch application for curve analyis
+    """
+    # my_os = sys.platform
+
+    app = QApplication.instance()
+    if not app:
+        app = QApplication(sys.argv)
+    view = View()
+    controller = Controller(view)
+    view.set_controller(controller)
+    view.show()
+    app.exec()
+
+START_TIME = time()
+PATH_FILE = Path('data_test' + os.sep + 'txt' + os.sep)
+PATH_FILE_JPK = Path("data_test" + os.sep + 'jpk_nt_force' + os.sep)
+main()
+print("--- %s seconds ---" % (time() - START_TIME))
