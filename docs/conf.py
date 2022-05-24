d1b44e00fcf91d887599347a60c83a61e4e3d3d2
TigiGln <thierry.galliano@etu.univ-amu.fr>
2022-05-24 12:28:53 +0200

    test readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ import otanalysis
 import main
 import setup_logger
 
+autodoc_mock_imports = ['sip', 'PyQt5', 'PyQt5.QtGui', 'PyQt5.QtCore', 'PyQt5.QtWidgets']
 # -- Project information -----------------------------------------------------
 
 project = 'OTAnalysis'
@@ -74,7 +75,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ['sip', 'PyQt5', 'PyQt5.QtGui', 'PyQt5.QtCore', 'PyQt5.QtWidgets']
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
