8e2a38881fde24c0797b1c793268ecead422a944
TigiGln <thierry.galliano@etu.univ-amu.fr>
2022-05-24 14:50:28 +0200

    jvfjyh

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='OTAnalysis',
     version='0.2.72',
-    packages=['OTAnalysis'],
+    packages=['otanalysis'],
     install_requires=[
         'requests',
         'importlib-metadata; python_version == "3.8"',
